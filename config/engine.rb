4662b692edf7edc80262b0dd7309425277b79563
Robert Glaser <mail@robert-glaser.de>
2012-11-20 10:34:36 +0100

    simplified engine migrations

--- config/engine.rb
+++ config/engine.rb
@@ -7,5 +7,9 @@ module Iqvoc
       paths["lib/tasks"] << "lib/engine_tasks"
     end
 
+    initializer "iqvoc_inflectionals.load_migrations" do |app|
+      app.config.paths['db/migrate'] += Iqvoc::Inflectionals::Engine.paths['db/migrate'].existent
+    end
+
   end
 end
