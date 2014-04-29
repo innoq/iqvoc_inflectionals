7be3cc1325d923fae98d6d8f335ce6dbf2b3c626
Robert Glaser <mail@robert-glaser.de>
2014-04-29 12:27:19 +0200

    fix line lengths

--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -1,10 +1,13 @@
 # initializer for both iqvoc and iqvoc_skosxl
 
-require 'iqvoc/xllabel' # XXX: this basically duplicates iqvoc_skosxl's initializer - but is required to ensure correct loading order!?
+# This basically duplicates iqvoc_skosxl's initializer —
+# but is required to ensure correct loading order!?
+require 'iqvoc/xllabel'
 
 Iqvoc.config.register_setting("title", "iQvoc Inflectionals")
 
-#Iqvoc.searchable_class_names << "Inflectional::Base" # XXX: application's responsibility!?
+# Application's responsibility!
+# Iqvoc.searchable_class_names << "Inflectional::Base"
 
 Iqvoc::XLLabel.additional_association_class_names.
     merge!("Inflectional::Base" => "label_id")
