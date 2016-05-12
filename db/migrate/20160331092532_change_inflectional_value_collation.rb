e972c7884aed3d10bfa4a16934bbe640bed27bc5
Marc Jansing <marc.jansing@innoq.com>
2016-05-12 11:51:40 +0200

    fix build if database adapter != mysql (/cc dlingstaedt)

--- db/migrate/20160331092532_change_inflectional_value_collation.rb
+++ db/migrate/20160331092532_change_inflectional_value_collation.rb
@@ -1,9 +1,13 @@
 class ChangeInflectionalValueCollation < ActiveRecord::Migration
   def up
-    execute("ALTER TABLE inflectionals MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_bin;")
+    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+      execute("ALTER TABLE inflectionals MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_bin;")
+    end
   end
 
   def down
-    execute("ALTER TABLE inflectionals MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci;")
+    if ActiveRecord::Base.connection.adapter_name == 'Mysql2'
+      execute("ALTER TABLE inflectionals MODIFY value varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci;")
+    end
   end
 end
