fe507a3e97fc4c107e8beade893b4a0eac10c474
Marc Jansing <marc.jansing@innoq.com>
2021-07-26 13:17:46 +0200

    switch to faucet

---
+++ app/assets/javascripts/manifest.js
@@ -0,0 +1,3 @@
+import '../../../node_modules/iqvoc/app/assets/javascripts/framework';
+import '../../../node_modules/iqvoc/app/assets/javascripts/iqvoc/manifest';
+import '../../../node_modules/iqvoc_skosxl/app/assets/javascripts/iqvoc_skosxl/manifest';
